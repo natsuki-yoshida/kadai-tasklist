6e9e2229c5f2b5214e8be8720f7eedb14bf40362
natsuki-yoshida <ec2-user@ip-172-31-34-30.us-east-2.compute.internal>
2018-06-04 03:41:52 +0000

    add column

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -3,8 +3,9 @@
 @section('content')
 
     <h1>id = {{ $task->id }} のタスク詳細ページ</h1>
-
-    <p>{{ $task->content }}</p>
+    <p>タイトル: {{ $task->status }}</p>
+    <p>メッセージ: {{ $task->content }}</p>
+   
 
     {!! link_to_route('tasks.edit', 'このタスク編集', ['id' => $task->id]) !!}
 
