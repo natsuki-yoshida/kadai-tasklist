a9fdf424af14084eee19a91ed135295cb73a896e
natsuki-yoshida <ec2-user@ip-172-31-34-30.us-east-2.compute.internal>
2018-06-06 05:26:26 +0000

    bootstrap design

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -11,24 +11,24 @@
             @endforeach
         </ul>
     @endif
-    <div class="row">
-        <div class="col-xs-6">
-
+         <div class="row">
+         <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
             {!! Form::model($task, ['route' => 'tasks.store']) !!}
-            
                 <div class="form-group">
                     {!! Form::label('status', 'ステータス:') !!}
                     {!! Form::text('status', null, ['class' => 'form-control']) !!}
                 </div>
                     
                 <div class="form-group">
-                    {!! Form::label('content', 'ステータス:') !!}
+                    {!! Form::label('content', 'タスク:') !!}
                     {!! Form::text('content', null, ['class' => 'form-control']) !!}
                 </div>
-                  </div>
-    </div>
-                
-                {!! Form::submit('投稿', ['class' => 'btn btn-primary']) !!}
+       {!! Form::submit('投稿', ['class' => 'btn btn-primary']) !!}
         
             {!! Form::close() !!}
+
+      </div>
+   
+   　　　     </div>         
+                
 @endsection
--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -4,8 +4,11 @@
 
     <h1>id: {{ $task->id }} のタスク編集ページ</h1>
 <div class="row">
-        <div class="col-xs-6">
-            {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
+          <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
+         {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
+
+          
+              
                 <div class="form-group">
                     {!! Form::label('status', 'ステータス:') !!}
                     {!! Form::text('status', null, ['class' => 'form-control']) !!}
@@ -15,9 +18,7 @@
                     {!! Form::label('content', 'タスク:') !!}
                     {!! Form::text('content', null, ['class' => 'form-control']) !!}
                 </div>
-        
-                {!! Form::submit('更新', ['class' => 'btn btn-default']) !!}
-        
+                {!! Form::submit('更新', ['class' => 'btn btn-default']) !!}               
             {!! Form::close() !!}
         </div>
     </div>
